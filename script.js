fca9263d1bb985dbcd2ea0d83ebba5bfd5256a26
Dana4V <112498@emmauscollege.nl>
2021-06-13 21:55:15 +0000

    laatste mini veranderingen

--- script.js
+++ script.js
@@ -18,21 +18,14 @@
 
 /**************************************************
 ** speltoestand **
-***************************************************/
+**************************************************/
 const UITLEG = 0;           // startscherm
 const SPELEN = 1;           // het spel
 const GAMEOVER = 2;         // eindscherm
 
-/**************************************************/
+/*************************************************/
 
 var spelStatus = 'INTRO';    // wat het spel nu doet
-
-
-/*************************************************
-** aantal meter **
-*************************************************/ 
-var frameTeller = 0;
-var aantalMeters = 0;
 
 
 
@@ -45,7 +38,7 @@ var spelerW = 70;          // breedte van speler x
 var spelerH = 70;          // hoogte van speler y 
 var spelerObject = { minx: spelerX - (spelerW/2),maxx: spelerX + (spelerW/2),maxy: spelerY + (spelerH/2),miny: spelerY - (spelerH/2)}
 
-var snelheidX = 10;
+var snelheidSpeler = 10;
 
 
 /************************************************
@@ -58,7 +51,6 @@ var vijandH = 100;          // hoogte van vijand y
 var vijanden = [];
 var snelheidVijand = 5;
 
-var snelheidY = 5;
 
 
 /*************************************************
@@ -71,17 +63,18 @@ const LEFT_ARROW = 37;
 const RIGHT_ARROW = 39;
 var spelBeginnen = false;
 
+
 /* ********************************************* */
 /*      functies die je gebruikt in je game      */
 /* ********************************************* */
 
 
 /**************************************************
- * Tekent het speelveld
- *************************************************/
+** Tekent het speelveld **
+**************************************************/
 var tekenVeld = function () {
-  fill("white");
-  rect(20, 20, 1240, 680);
+    fill("white");
+    rect(12, 7, 1260, 700);
 };
 
 
@@ -96,17 +89,17 @@ var tekenSpeler = function(x, y) {
     rect(spelerX-20, spelerY-20, 40, 40);
 
   // speler getekend
-  fill("grey");
-  ellipse(spelerX-25, spelerY-45, spelerW-20, spelerH-20);
-  ellipse(spelerX+155, spelerY-45, spelerW-20, spelerH-20);
+    fill("grey");
+    ellipse(spelerX-25, spelerY-45, spelerW-20, spelerH-20);
+    ellipse(spelerX+155, spelerY-45, spelerW-20, spelerH-20);
 
-  fill("blue");
-  rect(spelerX-20, spelerY-20, spelerW+100, spelerH+65);
-  rect(spelerX-50, spelerY-50, spelerW-20, spelerH+30);
-  rect(spelerX+130, spelerY-50, spelerW-20, spelerH+30);
+    fill("blue");
+    rect(spelerX-20, spelerY-20, spelerW+100, spelerH+65);
+    rect(spelerX-50, spelerY-50, spelerW-20, spelerH+30);
+    rect(spelerX+130, spelerY-50, spelerW-20, spelerH+30);
 
-  fill("grey");
-  ellipse(spelerX+65, spelerY+50, spelerW-20, spelerH+10);
+    fill("grey");
+    ellipse(spelerX+65, spelerY+50, spelerW-20, spelerH+10);
  
 };
 
@@ -115,105 +108,42 @@ var tekenSpeler = function(x, y) {
 
 
 
-// ---------------------------------------------------
-//    BEWEGING SPELER: pijlen en niet buiten scherm
-// ---------------------------------------------------
-var bewegingVijand = function(){
-    for (var i=0; i < vijanden.length; i++){
-        var eppo = vijanden[i];
-        eppo.y = eppo.y + snelheidVijand;
-        eppo.miny = eppo.miny + snelheidVijand;
-        eppo.maxy = eppo.maxy + snelheidVijand;
-        if ( eppo.y + (vijandH/2) > veldHoogte ){
-            eppo.y = 0-(vijandH/2);
-            eppo.miny = eppo.y - (vijandH/2);
-            eppo.maxy = eppo.y + (vijandH/2);
-            eppo.x = random(0, veldBreedte-(vijandW/2))
-            eppo.miny = eppo.x - (vijandW/2);
-            eppo.maxy = eppo.x + (vijandW/2);
-        }
-    }
-}
+/*****************************************************
+** BEWEGING SPELER: pijlen en niet buiten scherm **
+*****************************************************/
 
 var bewegingSpeler = function () {
 
-  // ----- pijlen op scherm -----
-  fill(255, 255, 255);
-  rect(435, 875, 50, 20); // --- pijl 1 ---
-  triangle(435, 905, 410, 885, 435, 865);
+    /********* pijltjestoetsen op toetsenbord **********/
+    if (keyIsPressed) {
+        if (keyCode === LEFT_ARROW) {
+        spelerX = spelerX - snelheidSpeler;
 
-  rect(515, 875, 50, 20); // --- pijl 2 ---
-  triangle(565, 905, 590, 885, 565, 865);
+        fill(random(100, 255), random(100, 255), random(100, 255));
+        rect(435, 875, 50, 20);
+        triangle(435, 905, 410, 885, 435, 865);
+        }
+        if (keyCode === RIGHT_ARROW) {
+        spelerX = spelerX + snelheidSpeler;
 
-
-  if (mouseIsPressed) {
-    if (mouseX < 500 && mouseX > 410 && mouseY < 905 && mouseY > 865) {
-      spelerX = spelerX - snelheidX;
-
-      fill(random(100, 255), random(100, 255), random(100, 255));
-      rect(435, 875, 50, 20);
-      triangle(435, 905, 410, 885, 435, 865);
-    }
-
-    if (mouseX < 590 && mouseX > 515 && mouseY < 905 && mouseY > 865) {
-      spelerX = spelerX + snelheidX;
-
-      fill(random(100, 255), random(100, 255), random(100, 255));
-      rect(515, 875, 50, 20);
-      triangle(565, 905, 590, 885, 565, 865);
-    }
-  }
-
-  // ----- pijltjestoetsen op toetsenbord -----
-  if (keyIsPressed) {
-    if (keyCode === LEFT_ARROW) {
-      spelerX = spelerX - snelheidX;
-
-      fill(random(100, 255), random(100, 255), random(100, 255));
-      rect(435, 875, 50, 20);
-      triangle(435, 905, 410, 885, 435, 865);
-    }
-    if (keyCode === RIGHT_ARROW) {
-      spelerX = spelerX + snelheidX;
-
-      fill(random(100, 255), random(100, 255), random(100, 255));
-      rect(515, 875, 50, 20);
-      triangle(565, 905, 590, 885, 565, 865);
-    }
-  }
-
-  // ----- speler niet buiten scherm -----
-  if (spelerX < 0) {
-    spelerX = 0;
-  }
-  if (spelerX > veldBreedte - (spelerW / 2)) {
-    spelerX = veldBreedte - (spelerW / 2);
-  }
-  spelerObject.minx = spelerX - (spelerW/2);
-  spelerObject.maxx = spelerX + (spelerW/2);
-
-}
-
-/******
- * test of we elkaar raken
- */
-
- var rakenElkaar = function (obj1, obj2){
-     var xoverlap = obj1.minx <= obj2.maxx && obj1.maxx >= obj2.minx;
-     var yoverlap = obj1.miny <= obj2.maxy && obj1.maxy >= obj2.miny;
-     return( xoverlap && yoverlap);
- }
-
-var raaktSpelerIets = function(){
-    var geraakt = false;
-    for (var i=0; i < vijanden.length; i++){
-        if ( rakenElkaar(spelerObject, vijanden[i])){
-            geraakt = true;
-            break;
+        fill(random(100, 255), random(100, 255), random(100, 255));
+        rect(515, 875, 50, 20);
+        triangle(565, 905, 590, 885, 565, 865);
         }
     }
-    return( geraakt );
-}
+
+    /******** speler niet buiten scherm *********/
+    if (spelerX < 0) {
+        spelerX = 0;
+    }
+    if (spelerX > veldBreedte - (spelerW / 2)) {
+        spelerX = veldBreedte - (spelerW / 2);
+    }
+    spelerObject.minx = spelerX - (spelerW/2);
+    spelerObject.maxx = spelerX + (spelerW/2);
+
+    }
+
 
 /*****************************************************
  * Tekent de vijand
@@ -238,45 +168,87 @@ var tekenVijand = function(x, y) {
 
 };
 
-
-// --------------------------------------------------
-//   STARTSCHERM: uitleg werkinng spel en startknop
-// --------------------------------------------------
-var startScherm = function () {
-  background(52, 125, 235);
-  opnieuw();
-
-  // ----- titel ------
-  
-  fill(random(100, 255), random(100, 255), random(100, 255));
-  textSize(100);
-  text("Enemydodger", 350, 100, 400,500);
-
-  // ----- startknop -----
-  rect(500, 455, 200, 90);
-  fill(0, 0, 0);
-  textSize(45);
-  text("START", 525, 475,400,500);
-  
-
-  if (mouseIsPressed) {
-    if (mouseX < 615 && mouseX > 385 && mouseY < 545 && mouseY > 455) {
-      spelStatus = "SPELEN"
+/*****************************************************
+** BEWEGING VIJAND **
+*****************************************************/
+var bewegingVijand = function(){
+    for (var i=0; i < vijanden.length; i++){
+        var eppo = vijanden[i];
+        eppo.y = eppo.y + snelheidVijand;
+        eppo.miny = eppo.miny + snelheidVijand;
+        eppo.maxy = eppo.maxy + snelheidVijand;
+        if ( eppo.y + (vijandH/2) > veldHoogte ){
+            eppo.y = 0-(vijandH/2);
+            eppo.miny = eppo.y - (vijandH/2);
+            eppo.maxy = eppo.y + (vijandH/2);
+            eppo.x = random(0, veldBreedte-(vijandW/2))
+            eppo.miny = eppo.x - (vijandW/2);
+            eppo.maxy = eppo.x + (vijandW/2);
+        }
     }
-  }
+}
+
+/*******************************************
+** test of vijand en speler elkaar raken **
+*******************************************/
+
+ var rakenElkaar = function (obj1, obj2){
+     var xoverlap = obj1.minx <= obj2.maxx && obj1.maxx >= obj2.minx;
+     var yoverlap = obj1.miny <= obj2.maxy && obj1.maxy >= obj2.miny;
+     return( xoverlap && yoverlap);
+ }
+
+var raaktSpelerIets = function(){
+    var geraakt = false;
+    for (var i=0; i < vijanden.length; i++){
+        if ( rakenElkaar(spelerObject, vijanden[i])){
+            geraakt = true;
+            break;
+        }
+    }
+    return( geraakt );
 }
 
 
-// -------------------------------------------
-//   EINDSCHERM: behaalde score en startknop
-// -------------------------------------------
+
+/******************************************************
+** STARTSCHERM **
+*******************************************************/
+var startScherm = function () {
+    background(52, 125, 235);
+    opnieuw();
+
+    /***** titel *****/
+    
+    fill(random(100, 255), random(100, 255), random(100, 255));
+    textSize(100);
+    text("Enemydodger", 350, 100, 400,500);
+
+    /***** startknop *****/
+    rect(500, 455, 200, 90);
+    fill(0, 0, 0);
+    textSize(45);
+    text("START", 525, 475,400,500);
+    
+
+    if (mouseIsPressed) {
+        if (mouseX < 615 && mouseX > 385 && mouseY < 545 && mouseY > 455) {
+        spelStatus = "SPELEN"
+        }
+    }
+}
+
+
+/**********************************************
+**  EINDSCHERM **
+**********************************************/
 var eindScherm = function () {
-  background(52, 125, 235);
+    background(52, 125, 235);
 
-  textSize(100);
-  text("GAME OVER", 475, 150, 400, 500);
+    textSize(100);
+    text("GAME OVER", 475, 150, 400, 500);
 
-    // ----- startknop -----
+    /***** startknop *****/
     fill(random(100, 255), random(100, 255), random(100, 255));
     rect(500, 455, 230, 90);
     fill(0, 0, 0);
@@ -299,23 +271,23 @@ var eindScherm = function () {
 
 
 
-// ------------------------------------
-//   GAME OVER: het spel is afgelopen
-// ------------------------------------
+/****************************************
+**  GAME OVER: het spel is afgelopen **
+****************************************/
 var gameOver = function () {
-  var returnWaarde = "verder";
-  var i = 0;
+    var returnWaarde = "verder";
+    var i = 0;
 
-  while (returnWaarde === "verder" && i < vijanden.length) {
+    while (returnWaarde === "verder" && i < vijanden.length) {
     if (spelerX < vijanden[i].x + 110 &&
-      spelerX + 75 > vijanden[i].x - 15 &&
-      spelerY + 20 < vijanden[i].y + 15 &&
-      spelerY + 20 > vijanden[i].y - 30 ||
-      spelerX < vijanden[i].x + 110 &&
-      spelerX + 75 > vijanden[i].x - 15 &&
-      spelerY + 125 < vijanden[i].y + 15 &&
-      spelerY + 125 > vijanden[i].y - 30) {
-      returnWaarde = "af";
+        spelerX + 75 > vijanden[i].x - 15 &&
+        spelerY + 20 < vijanden[i].y + 15 &&
+        spelerY + 20 > vijanden[i].y - 30 ||
+        spelerX < vijanden[i].x + 110 &&
+        spelerX + 75 > vijanden[i].x - 15 &&
+        spelerY + 125 < vijanden[i].y + 15 &&
+        spelerY + 125 > vijanden[i].y - 30) {
+        returnWaarde = "af";
     }
 
     i++;
@@ -325,27 +297,15 @@ var gameOver = function () {
 }
 
 
-// ---------------------------------------------------------
-//  DATA RESETTEN: snelheid, aantal meters, positie vijand
-// ---------------------------------------------------------
+/**************************************************************
+**  DATA RESETTEN: snelheid, positie vijand **
+**************************************************************/
 var opnieuw = function () {
-    snelheidY = 5;
-    aantalMeters = 0;
+    snelheidVijand = 5;
     maakVijanden();
    
 }
 
-
-
-
-/**
- * Zoekt uit of het spel is afgelopen
- * @returns {boolean} true als het spel is afgelopen
- */
-var checkGameOver = function() {
-    
-  return false;
-};
 
 
 /**************************************************************************
@@ -355,13 +315,11 @@ var checkGameOver = function() {
  * de p5 library, zodra het spel geladen is in de browser
 **************************************************************************
 **************************************************************************/
-
-
 function setup() {
-  frameRate(30);
-  createCanvas(veldBreedte, veldHoogte);
-  background(220, 225, 255);
-  //noStroke();
+    frameRate(30);
+    createCanvas(veldBreedte, veldHoogte);
+    background(220, 225, 255);
+  
 
     maakVijanden();
 }
@@ -383,7 +341,6 @@ var maakVijanden = function(){
  * uitgevoerd door de p5 library, nadat de setup functie klaar is
 ***************************************************************************
 **************************************************************************/
-
 function draw() {
 
     
